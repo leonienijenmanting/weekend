0b3eb061b2f4548aded11ccca70aca38a5ec3247
leonienijenmanting <61286346+leonienijenmanting@users.noreply.github.com>
2020-10-29 18:01:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const plannen = [
+  `ga wandelen`,
+  `trek een fles wijn open`,
+  `bel zomaar een vriendin op`,
+  `maak boerenkool`,
+  `lees een boek`,
+  `abonneer je op een tijdschrift`,
+  `leg een legpuzzel`,
+  `adopteer een kat`,
+  `bestel iets voor jezelf van minstens 100 euro`,
+  `maak een Japanse puzzel (nonogram in het Engels)`,
+  `verf een muur in je huis`,
+  `koop iets bij een kringloopwinkel`,
+  `ruim je kledingkast op`,
+  `koop bloemen, of een plant`,
+  `stuur een compliment naar iemand`,
+  `ga sporten`,
+  `zoek je droombaan`,
+  `mediteer`,
+  `houd een Netflix-avond`,
+  `plan een Monopoly-avond`
+];
+
+
+document.getElementById(`button`).onclick = function() {
+  const random = Math.floor(Math.random() * plannen.length);
+  document.getElementById(`weekendplan`).innerHTML = plannen[random];
+}
